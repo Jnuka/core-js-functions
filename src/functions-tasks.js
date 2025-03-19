5c95ddfccafc3d794df5302b82ba73c976aa42fb
Jnuka <jnuka@mail.ru>
2025-03-19 19:25:42 +0400

    feat: add partialUsingArguments()

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -177,8 +177,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function fn2(...args) {
+    return fn(...args1, ...args);
+  };
 }
 
 /**
