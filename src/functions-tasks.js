6cab3a085afce8c5d09c0a83e6778dfe74e2a987
Jnuka <jnuka@mail.ru>
2025-04-10 17:47:46 +0400

    feat: add getIdGeneratorFunction()

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -222,8 +222,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let Curid = startFrom - 1;
+  return () => {
+    Curid += 1;
+    return Curid;
+  };
 }
 
 module.exports = {
